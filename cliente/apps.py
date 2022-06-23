a6d4433f9c408993efb1a8dd3d890acb867c1937
JoseMario26 <108035969+JoseMario26@users.noreply.github.com>
2022-06-22 21:20:56 -0300

    Cliente

---
+++ cliente/apps.py
@@ -0,0 +1,43 @@
+from django.apps import AppConfig
+
+
+class ClienteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cliente'
+Matricula : int
+telefone : String
+cep : String
+Rua : Strind
+bairo : String
+cidade : string
+uf : String
+
+email : String
+Senha : String
+permissão : String
+
+CPF : String
+RG : String
+nome : String
+Sexo : String
+DataNacimento : String
+CarteiraTrabalho : Sting
+Salario : String
+Pis : String
+
++ consutar_imovel():Double
++ mater_Anucio(): void
++ mater_Cleinte():Void
++ manter_Funcionario():Void
++ mater_Agendamento():Void
++ gerar_Relatorio():void
++ calcular_salario():Double
+
+Comissa:int
+idCorretor:String
++Calcular_salario():Double
+
+- comissao:int
++ calcular_salario:() Double
++ calcular_salario:() Double
+
